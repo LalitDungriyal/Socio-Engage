69597c81e147fe8a5f04f17fcdee6c9489bcf996
Omanshu480 <omanshumahawar.181co237@nitk.edu.in>
2020-12-20 15:27:41 +0530

    Connecting to MongoDB Atlas

---
+++ server/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    MONGOURI: "mongodb+srv://omanshu840:WuBHdfi2jfhTvPSP@cluster0.ht6zp.mongodb.net/<dbname>?retryWrites=true&w=majority"
+}
--- server/server.js
+++ server/server.js
@@ -1,6 +1,23 @@
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose')
 const PORT = 3000
+const {MONGOURI} = require('./keys')
+
+mongoose.connect(MONGOURI)
+
+mongoose.connection.on('connected', () => {
+    console.log('Connected to Mongo')
+})
+
+mongoose.connection.on('error', (err) => {
+    console.log('Error Connecting ', err)
+})
+
+const customMiddleware = (req, res, next) => {
+    console.log('middleware executed')
+    next()
+}
 
 app.get('/', (req, res) => {
     res.send("hello World")
